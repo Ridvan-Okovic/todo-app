9194d265d4024f657dacb870420360bca279effa
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-09-07 10:03:00 +0200

    Removed unused context variables

--- client/src/context/EditProvider.jsx
+++ client/src/context/EditProvider.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react';
 import EditContext from './edit-context';
 const EditProvider = (props) => {
-  const [isEditing, setIsEditing] = useState(false);
   const [todo, setTodo] = useState({});
   const editContext = {
     todo,
     setTodo,
-    isEditing,
-    setIsEditing,
   };
   return (
     <EditContext.Provider value={editContext}>
